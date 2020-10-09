57159d294e722c647f967b19df0577eff3906766
Hai Pham <admin@tinhtify.com>
2020-10-09 14:25:03 +0100

    memorize Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { TouchableOpacity, Text } from 'react-native';
 
@@ -15,4 +15,4 @@ Button.propTypes = {
   text: PropTypes.string.isRequired,
 };
 
-export default Button;
+export default memo(Button);
